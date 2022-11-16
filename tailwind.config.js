b96d6f5d46949d2d51a993eab24cfd34bd6398cd
Xavybarcenas <57043767+Xavybarcenas@users.noreply.github.com>
2022-11-15 19:19:22 -0600

    add dependeces for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+      
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './componentes/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+      screens: {
+        'sm': '320px',
+        // => @media (min-width: 640px) { ... }
+        
+        'md': '375px',
+        // => @media (min-width: 275px) { ... }
+    
+        'l': '425px',
+        // => @media (min-width: 425px) { ... }
+  
+        'ls': '768px',
+        // => @media (min-width: 768px) { ... }
+        
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'hd': '1080px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    },
+    plugins: [],
+  }
+  
